83f2a1e9daa576209c24463eaffba2ee72e2e98b
yusufhalilyusuf <yusufhalilyusuf@hotmail.com>
2023-10-24 09:07:55 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import  ReactDOM  from "react-dom/client";
 
 import App from "./App.jsx";
 import "./App.css";
-import {  HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/fe-nc-news/">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
